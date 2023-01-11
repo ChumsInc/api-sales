fe5e9756e1bcd0bdbfc82f519528a9b9e81bc90a
Steve Montgomery <steve@chums.com>
2023-01-11 12:59:58 -0700

    Multiple updates and fixes.

---
+++ lib/pace/pace-types.ts
@@ -0,0 +1,28 @@
+import Decimal from "decimal.js";
+
+export interface PaceBudgetRow {
+    BudgetCode: string;
+    Account:string;
+    Amt: string|number|Decimal;
+}
+
+export interface PaceBudgetList {
+    [key:string]: {
+        ORIGINAL: string|number|Decimal;
+        REVISED: string|number|Decimal;
+    }
+}
+
+export interface DivisionPaceRow {
+    ARDivisionNo: string;
+    ARDivisionDesc: string;
+    Year: string|number;
+    Month: string|number;
+    budget: string|number|Decimal;
+    goal: string|number|Decimal;
+    InvoiceTotal: string|number|Decimal;
+    CurrentInvoiceTotal: string|number|Decimal;
+    PrevOpenOrderTotal: string|number|Decimal;
+    OpenOrderTotal: string|number|Decimal;
+    HeldOpenOrderTotal: string|number|Decimal;
+}
