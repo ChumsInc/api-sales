7163e7f6c0b6cf7286247d785b149f0083f49275
Steve Montgomery <steve@chums.com>
2023-05-08 12:39:10 -0600

    work in progress...

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-const dotenv  = require('dotenv').config();
-if (dotenv.error) {
-    console.log('*** error loading .env', dotenv.error);
-    return;
-}
+import 'dotenv/config.js';
+import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import http from 'node:http';
+import path from 'node:path';
+import {default as libRouter} from './lib/index.js';
+import Debug from 'debug';
 
-const debug = require('debug')('chums:index');
-const express = require('express');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const http = require('http');
-const path = require('path');
-const libRouter = require('./lib');
+const debug = Debug('chums:index');
 
 
 const app = express();
 app.set('trust proxy', 'loopback');
 app.use(helmet());
-// app.use(function (req, res, next) {
-//     res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
-//     next()
-// })
 app.set('json spaces', 2);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(libRouter.router);
+app.use(libRouter);
 
 const {PORT, NODE_ENV} = process.env;
 const server = http.createServer(app);
