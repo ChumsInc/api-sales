464ab838269f07fdf56e3c22fb289fb99ff38b05
Steve Montgomery <steve@chums.com>
2025-08-11 10:11:14 -0600

    (rep mismatch report): refactor to TypeScript, additional functionality as a demo for sending attachments to email

---
+++ lib/audits/sales-order/rep-mismatch/load-orders.ts
@@ -0,0 +1,96 @@
+import {mysql2Pool} from "chums-local-modules";
+import Debug from "debug";
+import type {RepMismatchRecord} from "./types.js";
+
+const debug = Debug('chums:lib:audits:sales-order:rep-mismatch:load-orders');
+
+export async function loadRepMismatch(): Promise<RepMismatchRecord[]> {
+    const sql = `SELECT c.ARDivisionNo,
+                        c.CustomerNo,
+                        NULL                                                         AS ShipToCode,
+                        c.CustomerName,
+                        NULL                                                         AS ShipToName,
+                        CONCAT_WS('-', c.SalespersonDivisionNo, c.SalespersonNo)     AS CustomerRep,
+                        rep.SalespersonName,
+                        soh.SalesOrderNo,
+                        soh.OrderType,
+                        soh.OrderDate,
+                        soh.ShipExpireDate,
+                        CONCAT_WS('-', soh.SalespersonDivisionNo, soh.SalespersonNo) AS SalesOrderRep,
+                        sorep.SalespersonName                                        AS SalesOrderRepName
+                 FROM c2.SO_SalesOrderHeader soh
+                          INNER JOIN c2.ar_customer c USING (Company, ARDivisionNo, CustomerNo)
+                          LEFT JOIN c2.ar_salesperson rep ON rep.Company = c.Company AND
+                                                             rep.SalespersonDivisionNo = c.SalespersonDivisionNo AND
+                                                             rep.SalespersonNo = c.SalespersonNo
+                          LEFT JOIN c2.ar_salesperson sorep ON sorep.Company = soh.Company AND
+                                                               sorep.SalespersonDivisionNo =
+                                                               soh.SalespersonDivisionNo AND
+                                                               sorep.SalespersonNo = soh.SalespersonNo
+                 WHERE soh.Company = 'chums'
+                   AND soh.ShipToCode IS NULL
+                   AND ((CONCAT_WS('-', soh.SalespersonDivisionNo, soh.SalespersonNo) <>
+                         CONCAT_WS('-', c.SalespersonDivisionNo, c.SalespersonNo))
+                     OR IFNULL(sorep.UDF_TERMINATED, 'N') = 'Y')
+                   AND sorep.SalesManagerNo <> rep.SalespersonNo
+                   AND c.SalespersonNo NOT IN ('H00E', 'H00W', 'TEST', '0000', 'H04', 'H01', 'H00')
+                   AND soh.OrderType <> 'Q'
+
+                 UNION
+
+                 SELECT soh.ARDivisionNo,
+                        soh.CustomerNo,
+                        soh.ShipToCode,
+                        soh.BillToName      AS CustomerName,
+                        soh.ShipToName,
+                        st.CustomerRep,
+                        st.SalespersonName,
+                        soh.SalesOrderNo,
+                        soh.OrderType,
+                        soh.OrderDate,
+                        soh.ShipExpireDate,
+                        soh.SalesOrderRep,
+                        soh.SalespersonName AS SalesOrderRepName
+                 FROM (SELECT h.*,
+                              CONCAT_WS('-', sp.SalespersonDivisionNo, sp.SalespersonNo)   AS SalesOrderRep,
+                              CONCAT_WS('-', sp.SalesManagerDivisionNo, sp.SalesManagerNo) AS SalesManager,
+                              sp.SalespersonName,
+                              sp.UDF_TERMINATED
+                       FROM c2.SO_SalesOrderHeader h
+                                LEFT JOIN c2.ar_salesperson sp ON sp.Company = h.Company AND
+                                                                  sp.SalespersonDivisionNo = h.SalespersonDivisionNo AND
+                                                                  sp.SalespersonNo = h.SalespersonNo) soh
+                          INNER JOIN (SELECT st.*,
+                                             CONCAT_WS('-', sp.SalespersonDivisionNo, sp.SalespersonNo)   AS CustomerRep,
+                                             CONCAT_WS('-', sp.SalesManagerDivisionNo, sp.SalesManagerNo) AS SalesManager,
+                                             sp.SalespersonName,
+                                             sp.UDF_TERMINATED
+                                      FROM c2.SO_ShipToAddress st
+                                               LEFT JOIN c2.ar_salesperson sp ON sp.Company = st.Company AND
+                                                                                 sp.SalespersonDivisionNo =
+                                                                                 st.SalespersonDivisionNo AND
+                                                                                 sp.SalespersonNo = st.SalespersonNo) st
+                                     ON st.Company = soh.Company AND
+                                        st.ARDivisionNo = soh.ARDivisionNo AND
+                                        st.CustomerNo = soh.CustomerNo AND
+                                        st.ShipToCode = IFNULL(soh.ShipToCode, '')
+                 WHERE soh.Company = 'chums'
+                   AND soh.OrderType <> 'Q'
+                   AND st.SalespersonNo NOT IN ('H00E', 'H00W', 'TEST', '0000', 'H04', 'H01', 'H00')
+                   AND (NOT (st.CustomerRep = soh.SalesOrderRep OR st.SalesManager = soh.SalesManager) OR
+                        IFNULL(soh.UDF_TERMINATED, 'N') = 'Y')
+
+                 ORDER BY ARDivisionNo, CustomerNo, SalesOrderNo`;
+    try {
+        const [rows] = await mysql2Pool.query<RepMismatchRow[]>(sql);
+        return rows;
+    } catch (err) {
+        debug('loadRepMismatch()', err);
+        if (err instanceof Error) {
+            debug("loadRepMismatch()", err.message);
+            return Promise.reject(err);
+        }
+        debug("loadRepMismatch()", err);
+        return Promise.reject(new Error('Error in loadRepMismatch()'));
+    }
+}
