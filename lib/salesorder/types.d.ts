a49727b223ab6121d6af255d3961039f991c622c
Steve Montgomery <steve@chums.com>
2025-10-10 08:11:40 -0600

    feat: implement `open-order-margins` and `orders-by-period` modules, including detailed SQL queries and data processing for sales order and invoice margins

--- lib/salesorder/types.d.ts
+++ lib/salesorder/types.d.ts
@@ -6,3 +6,36 @@ export interface LoadOrdersParams {
     limit?: number|string;
     offset?: number|string;
 }
+
+export interface OrdersByPeriodRecord {
+    SalesOrderNo: string;
+    OrderDate: string;
+    OrderType: string;
+    CancelReasonCode: string|null;
+    CancelReasonCodeDesc: string|null;
+    ShipExpireDate: string|null;
+    UDF_IMPRINTED: string|null;
+    ARDivisionNo: string;
+    CustomerNo: string;
+    BillToName: string|null;
+    BillToCity: string|null;
+    BillToState: string|null;
+    BillToCountryCode: string|null;
+    ShipToCode: string|null;
+    ShipToName: string|null;
+    ShipToCity: string|null;
+    ShipToState: string|null;
+    ShipToCountryCode: string|null;
+    CurrentInvoiceNo: string|null;
+    OrderTotal: string|number;
+    LastInvoiceNo: string|null;
+    LastInvoiceDate: string|null;
+    SalespersonDivisionNo: string|null;
+    SalespersonNo: string|null;
+    CreatedByUser: string|null;
+    UpdatedByUser: string|null;
+    OrderWeek: string|number;
+    ShipWeek: string|number;
+    week: number;
+    day: number;
+}
